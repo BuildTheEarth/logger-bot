9520f60a19ea832524d6d89470234d3ddaa6f0e0
Malachi <malachi.evan@icloud.com>
2022-02-22 18:24:58 -0700

    Update messageReactionDelete.js

--- events/messageReactionDelete.js
+++ events/messageReactionDelete.js
@@ -3,6 +3,6 @@ export default {
     once: false,
     execute(reaction, user, client) {
         client.logger.info("lol")
-        //log a what reaction was delted to what message and by who
+        //log a what reaction was deleted to what message and by who
     }
 }
