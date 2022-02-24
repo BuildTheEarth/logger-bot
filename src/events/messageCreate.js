74998609dc304ba5864d74f086b97b1908332e53
Malachi Brooks <malachi@mebrooks.dev>
2022-02-24 12:37:20 -0700

    ping pong

---
+++ src/events/messageCreate.js
@@ -0,0 +1,26 @@
+import prettyMs from "pretty-ms"
+
+export default {
+    name: "messageCreate",
+    once: false,
+    async execute(message, client) {
+        //not creating a proper command handler for this because it only for super basic commands
+        if (message.author.bot) return
+        const msg = message.content.toLowerCase()
+        if (!message.content.startsWith(client.config.prefix)) return
+        const args = message.content.slice(client.config.prefix.length).trim().split(/ +/)
+        const command = args.shift().toLowerCase()
+
+        if (/[a-z]?ing/.test(command)) {
+            const embed = {
+                color: client.config.colors.join,
+                title: `${command.charAt(0).toUpperCase()}ong`,
+                description: `🏓Latency is ${Math.round(
+                    client.ws.ping
+                )} ms.\nUptime: ${prettyMs(client.uptime)}`,
+                timestamp: new Date()
+            }
+            message.channel.send({ embeds: [embed] })
+        }
+    }
+}
